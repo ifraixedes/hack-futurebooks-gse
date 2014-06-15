c7f0acca56a76da468a9568d7c6adda8e9b768c2
Ivan Fraixedes <ifcdev@gmail.com>
2014-06-15 10:02:42 +0100

    Added route and model (backend) for book screen

---
+++ dev/routes/book.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (config) {
+  var Gossip = require(config.rootPath + '/models/gossip')(config);
+  var Book = require(config.rootPath + '/models/book')(config);
+  var router = require('express').Router();
+
+
+  router.get('/:bookIsbn/goosipId?', function (req, res, next) {
+    Book.findByIsbn(req.params.bookIsbn)
+    .then(function (bookDoc) {
+      if (req.params.gossipId) {
+        return Gossip.findById(req.params.gossipId)
+        .then(function (gossipDoc) {
+          res.render('book', { book: bookDoc._source, gossip: gossipDoc });
+        });
+      } else {
+        res.render('book', { book: bookDoc._source });
+      }
+    }, next);
+  });
+
+  return router;
+};
